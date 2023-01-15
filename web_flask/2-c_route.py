df436913c5bca45b19e768998b2621421977c844
Mogbonjubola Favour <105045421+favour-22@users.noreply.github.com>
2023-01-16 00:41:02 +0100

    Update 2-c_route.py

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,1 +1,21 @@
+from flask import Flask, render_template
 
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret'
+app.config['strict_slashes'] = False
+
+@app.route('/', methods=['GET'])
+def index():
+    return "Hello HBNB!"
+
+@app.route('/hbnb', methods=['GET'])
+def hbnb():
+    return "HBNB"
+
+@app.route('/c/<text>', methods=['GET'])
+def c(text):
+    text = text.replace("_", " ")
+    return "C " + text
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=5000)
